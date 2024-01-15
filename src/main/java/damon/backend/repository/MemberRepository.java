ee1985b0ba2f845ec52e11099a929191e90c6ced
jungbin97 <ljb5054@gmail.com>
2024-01-15 16:44:15 +0900

    fix : 초기 설정 수정

--- src/main/java/damon/backend/repository/MemberRepository.java
+++ src/main/java/damon/backend/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package damon.backend.repository;
 
-import damon.backend.model.Member;
+
+import damon.backend.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
