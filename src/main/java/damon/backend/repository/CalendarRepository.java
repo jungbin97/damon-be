806781fb214c1862dc3b492aecced5853be8796b
jungbin97 <ljb5054@gmail.com>
2024-01-15 23:00:43 +0900

    feat : 레포지토리 생성

---
+++ src/main/java/damon/backend/repository/CalendarRepository.java
@@ -0,0 +1,9 @@
+package damon.backend.repository;
+
+import damon.backend.entity.Calendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+}
