a716f509e4f3c704ff90986ca19c5fe2c15a7787
jungbin97 <ljb5054@gmail.com>
2024-01-15 23:03:17 +0900

    feat : createCalendar테스트 코드 작성

--- src/main/java/damon/backend/entity/Member.java
+++ src/main/java/damon/backend/entity/Member.java
@@ -2,6 +2,7 @@ package damon.backend.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Entity
 @Table(name = "member")
 @Getter
+@Setter
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
