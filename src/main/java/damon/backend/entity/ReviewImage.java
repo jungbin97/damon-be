0e7cf5080fd77e62dce16e9fa32f86da9d3c2b97
jungbin97 <ljb5054@gmail.com>
2024-01-15 11:48:10 +0900

    feat : 엔티티 추가

---
+++ src/main/java/damon/backend/entity/ReviewImage.java
@@ -0,0 +1,46 @@
+package damon.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewImage {
+    //not null 이 너무 많아서 기본값을 not null로 설정
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    public @interface NotNull {
+        boolean nullable() default false;
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_image_id")
+
+    private Long id;
+    private String name;
+    private String url;
+    private boolean main;
+
+    //리뷰id 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    //연관관계 매핑 메서드
+    public void addReview(Review review){
+        this.review = review;
+        if (review != null ) {
+            review.getReviewImages().add(this);
+        }
+    }
+}
