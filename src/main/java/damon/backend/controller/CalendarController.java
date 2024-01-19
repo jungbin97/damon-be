1e58ef688bcc7f6cf28c1a4c3080f538f2ea0621
jungbin97 <ljb5054@gmail.com>
2024-01-19 15:57:10 +0900

    hotfix : cors 문제 설정값

--- src/main/java/damon/backend/controller/CalendarController.java
+++ src/main/java/damon/backend/controller/CalendarController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CalendarController {
     private final CalendarService calendarService;
 
