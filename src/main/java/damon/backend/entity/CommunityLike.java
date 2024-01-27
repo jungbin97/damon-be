a294ee09aac2b9787f2654a3b2e7b4ca14671e4a
g6y116 <g6y116@naver.com>
2024-01-28 01:05:57 +0900

    feat : 엔티티 추가

---
+++ src/main/java/damon/backend/entity/CommunityLike.java
@@ -0,0 +1,31 @@
+package damon.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "community_like")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommunityLike extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "community_like_id")
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_id")
+    private Community community;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public CommunityLike(Community community, Member member) {
+        this.community = community;
+        this.member = member;
+    }
+}
