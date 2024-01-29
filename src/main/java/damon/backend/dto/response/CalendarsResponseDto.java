c658c321b38eea4ec6ac337fff7ec1bebdc6555b
jungbin97 <ljb5054@gmail.com>
2024-01-29 19:57:56 +0900

    Revert "feat : 내 일정 리스트 조회 여행지 지역명 추가"
    This reverts commit c5a066b6460d2884bc585579454fbfd1c82916df.

--- src/main/java/damon/backend/dto/response/CalendarsResponseDto.java
+++ src/main/java/damon/backend/dto/response/CalendarsResponseDto.java
@@ -1,6 +1,5 @@
 package damon.backend.dto.response;
 
-import damon.backend.entity.Area;
 import damon.backend.entity.Calendar;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -18,15 +17,13 @@ public class CalendarsResponseDto {
     private String title;
     private LocalDate startDate;
     private LocalDate endDate;
-    private Area area;
 
     public static CalendarsResponseDto from(Calendar calendar) {
         return new CalendarsResponseDto(
                 calendar.getId(),
                 calendar.getTitle(),
                 calendar.getStartDate(),
-                calendar.getEndDate(),
-                calendar.getArea()
+                calendar.getEndDate()
         );
     }
 
